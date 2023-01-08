3806f0824617b9f6e2d0a0babb6e08ebd91e7cb0
astoucisse <astouc676@gmail.com>
2023-01-08 21:33:09 +0100

    astou

---
+++ src/test/java/com/ca/formation/formationdemo1/exception/ResourceNotFoundExceptionTest.java
@@ -0,0 +1,17 @@
+package com.ca.formation.formationdemo1.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResourceNotFoundExceptionTest {
+    @Test
+    void testConstructor() {
+        ResourceNotFoundException actualResourceNotFoundException = new ResourceNotFoundException("An error occurred");
+        assertNull(actualResourceNotFoundException.getCause());
+        assertEquals(0, actualResourceNotFoundException.getSuppressed().length);
+        assertEquals("An error occurred", actualResourceNotFoundException.getMessage());
+        assertEquals("An error occurred", actualResourceNotFoundException.getLocalizedMessage());
+    }
+
+}
